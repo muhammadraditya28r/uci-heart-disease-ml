d209140375d873e5df02839341385997169bd15e
iRasionize <irasionize@gmail.com>
2026-07-28 10:13:46 +0700

    Create project configuration

---
+++ src/heart_disease/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+DATA_DIR = PROJECT_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+MODEL_DIR = PROJECT_ROOT / "models"
+REPORT_DIR = PROJECT_ROOT / "report"
+
+RANDOM_STATE = 42
+TARGET_COLUMN = 'target'
