93f82e9fae3601230500471070bb56e75eef55bd
iRasionize <irasionize@gmail.com>
2026-09-01 00:02:31 +0700

    Edit data ingestion script and preprocessing notebook

--- src/heart_disease/data/ingestion.py
+++ src/heart_disease/data/ingestion.py
@@ -1,15 +1,20 @@
-from pathlib import Path
+from collections.abc import Callable
 
+from pathlib import Path
 import pandas as pd
 
 
-def load_csv(path: str | Path) -> pd.DataFrame:
+
+def load_file(path: str | Path,
+              read_func: Callable | None = None,
+              ) -> pd.DataFrame:
     """
-    Load a CSV file into a pandas DataFrame
+    Load a file into a pandas DataFrame
 
     Parameters
     ----------
-    path: Path to the CSV file.
+    path: Path to the file.
+    read_func: pandas read file method
 
     Returns
     -------
@@ -21,6 +26,17 @@ def load_csv(path: str | Path) -> pd.DataFrame:
     if not path.exists():
         raise FileNotFoundError(f"Data file not found: {path}")
     
-    return pd.read_csv(path)
+    if read_func == None:        
+        suffix = path.suffix.lower()
 
-    
+        if suffix == ".csv":
+            return pd.read_csv(path)
+
+        if suffix == ".parquet":
+            return pd.read_parquet(path)
+        
+        raise ValueError(f"Unsupported file type {suffix}, try using read_func")
+
+    else: 
+        return read_func(path)
+
