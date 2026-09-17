38285aff523eff2b4e4024415498938e836e84dd
iRasionize <irasionize@gmail.com>
2026-09-17 17:39:52 +0700

    feat: add verification module for final model

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
+from sklearn.base import ClassifierMixin
 
 from heart_disease.config import (
     RAW_DATA_DIR,
@@ -19,10 +20,11 @@ from heart_disease.features.preprocessing import (
 from heart_disease.models.train import save_model, train_model
 from heart_disease.models.tracking import log_model
 
+
 config = ExperimentConfig()
 
 
-def build_model(config: ExperimentConfig) -> Pipeline:
+def build_model(config: ExperimentConfig, classifier: ClassifierMixin) -> Pipeline:
     """Build the selected preprocessing + model pipeline"""
 
     preprocessor = create_preprocessor(
@@ -34,8 +36,6 @@ def build_model(config: ExperimentConfig) -> Pipeline:
             add_indicator=config.categorical_missing_indicator,
         ),
     )
-
-    classifier = LogisticRegression(**config.logistic_regression_params)
 
     return create_training_pipeline(
         classifier,
@@ -51,13 +51,17 @@ def main() -> None:
     X = df[FEATURES]
     y = df[TARGET_COLUMN]
 
-    model = build_model(config=config)
+    model = build_model(
+        config=config,
+        classifier=LogisticRegression(**config.logistic_regression_params),
+    )
 
     model = train_model(model, X, y)
 
     save_model(model, MODEL_DIR / "01_final_lr_model.joblib")
 
     log_model(model, name="production_model")
+
 
 if __name__ == "__main__":
     main()
---
+++ scripts/verification.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from numpy import nan
+
+from heart_disease.models.train import load_model
+from heart_disease.config import MODEL_DIR
+
+
+model = load_model(path=MODEL_DIR / "01_final_lr_model.joblib")
+
+
+print(model)
+
+
+sample = pd.DataFrame(
+    [
+        {
+            "age": 63,
+            "trestbps": 145,
+            "chol": 233,
+            "thalch": 150,
+            "oldpeak": 2.3,
+            "sex": 1,
+            "cp": "typical angina",
+            "fbs": nan,
+            "restecg": "lv hypertrophy",
+            "exang": 0,
+            "slope": "downsloping",
+            "ca": 0,
+            "thal": "fixed defect",
+        }
+    ]
+)
+
+prediction = model.predict(sample)
+
+print(prediction)
